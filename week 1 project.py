a712a39d4aa9914e8bf2069dc5b1337a81ff00da
MAGESGWARI M <mageshwarim9003998405@gmail.com>
2026-05-10 18:23:47 +0530

    week 1 rivision.py

--- week 1 project.py
+++ week 1 project.py
@@ -1,4 +1,4 @@
-print("SIMPLE CALCULATOR")
+print("===SIMPLE CALCULATOR===")
 num1=int(input("Enter first number:"))
 num2=int(input("Enter second number:"))
 print("\n 1.Addition \n 2.Subtraction \n 3.Multiplication \n 4.Division")
